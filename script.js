715861bbe944741eb44b6b4cff541e7b6598b1ac
Yves Habchi <yves.habchy@gmail.com>
2023-01-20 11:01:55 +0200

    Add comments and minor change in code I added comments for documentation and a minor change in the code(in the game()function I added a console.log for a draw outcome).

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getcomputerchoice()
+function getcomputerchoice()//gets the computer's selection
 {
     let choice;
     const arrofchoices=['rock','paper','scissors'];//possible choices for computer
@@ -6,87 +6,88 @@ function getcomputerchoice()
     choice=arrofchoices[random];//randomly picks an element in the array of choices(arrofchoices)
     return choice;
 }
-function playround(playerselection,computerselection)
+function playround(playerselection,computerselection)//plays 1 round of the game
 {
     let win=false;
     let draw=false;
-    let lose=false;
-    let result;
-    while(isValid(playerselection)===false)
+    let lose=false;//win,lose and draw are all false since the round has not begun yet
+    let result;//this will contain the value that will be returned
+    while(isValid(playerselection)===false)//if the answer is invalid
     {
-        playerselection=prompt("Enter a valid answer(rock-paper-scissors");
+        playerselection=prompt("Enter a valid answer(rock-paper-scissors)");//keep repeating till user enters a valid answer
     }
-    let ps=playerselection.toLowerCase();
-if(ps==='rock')
+    let ps=playerselection.toLowerCase();//turn the user answer to lower case 
+if(ps==='rock')//if user entered rock
 {
     if(computerselection==='scissors')
-    win=true;
+    win=true;//rock beats scissors
     else if(computerselection==='paper')
-    lose=true;
+    lose=true;//paper beats rock
     else
-    draw=true;
+    draw=true;//tie, the user and the computer both have the same value(draw)
 }
 else if(ps==='paper')
 {
     if(computerselection==='rock')
-    win=true;
+    win=true;//paper beats rock
     else if(computerselection==='scissors')
-    lose=true;
+    lose=true;//scissors beats paper
     else
     draw=true;
 }
-else
+else//scissors
 {
     if(computerselection==='paper')
-    win=true;
+    win=true;//scissors beats paper
     else if(computerselection==='rock')
-    lose=true;
+    lose=true;//rock beats scissors
     else
     draw=true;
 }
 
-if(win===true)
+if(win===true)//if user wins
 {
 console.log(`You win!${playerselection} beats ${computerselection}`);
 result=1;
 }
-else if(lose===true)
-{
-    
+else if(lose===true)//if user loses
+{ 
 console.log(`You lose.${computerselection} beats ${playerselection}`);
 result=-1;
 }
-else
+else//if it's a tie
 {
-console.log(`Draw`);
+console.log(`Draw.`);
 result=0;
 }
-return result;
+return result;//return 1 if user wins, 2 if user loses and 0 if it's a draw
 }
-function isValid(playerselection)
+function isValid(playerselection)//check if the answer enter by the user is valid
 {
-    if(playerselection.toLowerCase()==='rock' || playerselection.toLowerCase()==='paper' || playerselection.toLowerCase()==='scissors')
-    return true;
-    return false;
+    if(playerselection.toLowerCase()==='rock' || playerselection.toLowerCase()==='paper' || playerselection.toLowerCase()==='scissors')//answer has to be one of rock,paper or scissors
+    return true;//return true if valid
+    return false;//return false if invalid 
 }
 
-function game()
+function game()//plays 5 rounds of the game
 {
     let ps;
-    let userscore=0;
-    let computerscore=0;
-    for(let i=0;i<5;i++)
+    let userscore=0;//initial score of the user
+    let computerscore=0;//initial score of the computer
+    for(let i=0;i<5;i++)//runs 5 times
     {
         ps=prompt('Enter an answer');
-        pr=playround(ps,getcomputerchoice());
-        if(pr===1)
+        pr=playround(ps,getcomputerchoice());//store the playround() function in a variable named pr
+        if(pr===1)//if playround() returns 1=>user wins=>increment score for user
         userscore++;
-        else if(pr===-1)
+        else if(pr===-1)//if playround() returns -1=>user loses=>increment score for computer
         computerscore++;
     }
-    if(userscore>computerscore)
+    if(userscore>computerscore)//if the total score of the user is greater than that of the computer's
     console.log(`YOU WIN.Score:${userscore}-${computerscore}`);
-    else
+    else if(userscore<computerscore)//if the total score of the user is less than that of the computer's
     console.log(`YOU LOSE.Score:${userscore}-${computerscore}`);
+    else//if the user and the computer have the same score 
+    console.log(`DRAW.Score:${userscore}-${computerscore}`);
 }
 game();
